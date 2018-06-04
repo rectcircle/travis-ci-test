0e3e060b19f96b99e2033781963d06d8b3c0fff6
Rectcircle <sunben960729@163.com>
2018-06-04 20:19:08 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2018 rectcircle
+ *
+ * @file main.cpp 
+ * @author rectcircle 
+ * @date 2018-06-04 
+ * @version 0.0.1
+ */
+#include <stdio.h>
+
+int main(){
+    printf("Hello World\n");
+    return 0;
+}
+
